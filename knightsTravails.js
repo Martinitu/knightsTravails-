4f4f9847669892a6d80af3a22395e4db870fc2f9
Martinitu <martinitu@gmail.com>
2023-03-23 16:09:46 -0500

    gameboard and knight created

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,1 +1,14 @@
+function knightsTravails(){
+    let gameBoard = []
+    for (let i = 0; i < 8; i++){
+        gameBoard[i] = []
+       for (let j = 0; j < 8; j++) {
+           gameBoard[i][j] = [i,j]
+       }
+    }
+    let knight = [0,0]
 
+    console.log(gameBoard[0][0], knight)
+
+}
+knightsTravails()
